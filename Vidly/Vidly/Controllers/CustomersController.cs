7b146b896af9c03c7ed5072e0273af987e592c1b
zebby <zebbykhairah@gmail.com>
2019-04-12 16:33:52 +0800

    #3: chores

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
-using System.Linq;
 
 namespace Vidly.Controllers
 {
---
+++ Vidly/Vidly/Models/MyDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Vidly.Models
+{
+    public class MyDBContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
