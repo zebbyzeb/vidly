72b0140a14ec7f1942d8fb90a34f41ac6ff38a84
zebbykhairah <zebbykhairah@gmail.com>
2019-04-13 19:14:36 +0800

    #3: Seeding the Database: added MembershipTypes model and added customer and membershipTypes models to DB

--- Vidly/Vidly/Models/MyDBContext.cs
+++ Vidly/Vidly/Models/MyDBContext.cs
@@ -9,5 +9,6 @@ namespace Vidly.Models
     public class MyDBContext:DbContext
     {
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
     }
 }
